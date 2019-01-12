7639677174803f434a2e3b9b139dc89e5655680d
Mohit Munjani <mohitcrox@gmail.com>
2019-01-12 06:58:24 +0100

    fiz

--- parse.rb
+++ parse.rb
@@ -2,9 +2,14 @@ require 'json'
 files = `ls spotify_recently_played*`.split("\n")
 @result = {}
 files.each do |file|
-  json = JSON.parse(File.read(file).split("time_namelookup")[0])
-  json["items"].each do | item|
-    @result[item["played_at"]] = item
+  puts "parsing #{file}"
+  json = JSON.parse(File.read(file, :encoding => 'UTF-8').split("time_namelookup")[0])
+  if json["items"]
+    json["items"].each do | item|
+      @result[item["played_at"]] = {item: item, track: item["track"]["name"]}
+    end
+  else
+    puts "ERROR items is nil for #{file}"
   end
 end
 f = File.open("final_res.json", "wb")
